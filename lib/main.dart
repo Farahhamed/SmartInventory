1387fafe4f3a71fd49445e7f370be25efe97d243
nouranessam <juviaessam@gmail.com>
2024-01-14 20:07:29 +0200

    ai api is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:smartinventory/screens/PredictScreen.dart';
 import 'package:smartinventory/screens/ProductsScreen.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: ProductsScreen(),
+    //home: ProductsScreen(),
+    home: PredictScreen(),
   ));
 }
