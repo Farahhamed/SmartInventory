9e9eea8decc27c49c3dc7f5936e250452c6e50cb
Hamza08441 <hamza2008441@miuegypt.edu.eg>
2024-02-22 22:38:32 +0200

    fixed signup

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smartinventory/firebase_options.dart';
 import 'package:smartinventory/screens/Dashboard/ForecastingScreen.dart';
 import 'package:smartinventory/screens/ProductsList.dart';
 import 'package:smartinventory/screens/ProductsScreen.dart';
 import 'package:smartinventory/screens/SignUpScreen.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  runApp(MaterialApp(
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ChartWidget(),
+    home: SignUpScreen(),
   ));
 }
