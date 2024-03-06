3243d2674f3fac3a3bb03018aa9b1da1a1e7bfdd
Hamza08441 <hamza2008441@miuegypt.edu.eg>
2024-03-06 20:38:12 +0200

    fixed provider for signup

--- FlutterApp/lib/main.dart
+++ FlutterApp/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:smartinventory/firebase_options.dart';
+import 'package:smartinventory/providers/provider.dart';
 import 'package:smartinventory/screens/Dashboard/ForecastingScreen.dart';
 import 'package:smartinventory/screens/LoginScreen.dart';
 import 'package:smartinventory/screens/NavigationBarScreen.dart';
 import 'package:smartinventory/screens/Notification.dart';
 import 'package:smartinventory/screens/ProductsList.dart';
 import 'package:smartinventory/screens/ProductsScreen.dart';
-import 'package:smartinventory/screens/ProfileScreen.dart';
 import 'package:smartinventory/screens/SignUpScreen.dart';
 import 'package:smartinventory/screens/StockControl/ABCScreen.dart';
 import 'package:smartinventory/screens/StockControl/stockControlScreen.dart';
@@ -17,8 +18,11 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ProfileScreen(),
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
+    child: const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    ),
   ));
 }
