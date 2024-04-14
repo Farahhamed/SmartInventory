a73216d06be1df62d311ebf510ec3b034cfcd10e
Lojain Mostafa <lojain17mostafa@gmail.com>
2024-04-14 21:19:38 +0200

    home page ui app (appbar)

--- FlutterApp/lib/screens/Homepage.dart
+++ FlutterApp/lib/screens/Homepage.dart
@@ -9,137 +9,108 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePageScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePageScreen extends StatefulWidget {
+  const HomePageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageScreen> createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Home', style: TextStyle(fontSize: 20)),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://via.placeholder.com/150'), // Placeholder image URL
-            ),
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200.0),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(180),
+                  bottomRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: Image.asset(
+                  'assets/images/appbar.png',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+              ),
+              Positioned.fill(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(180),
+                    bottomRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.4),
+                  ),
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(100),
+                  bottomRight: Radius.circular(20),
+                ),
+                child: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  title: Center(
+                    child: Text(
+                      'Home',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  leading: IconButton(
+                    icon: Icon(Icons.menu),
+                    color: Colors.white,
+                    onPressed: () {
+                      // Open side menu
+                    },
+                  ),
+                  actions: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(right: 16.0),
+                      child: IconButton(
+                        icon: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 2.0,
+                            ),
+                          ),
+                          child: CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/images/kitty.png'),
+                          ),
+                        ),
+                        onPressed: () {
+                          // Navigate to profile page
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 10), // Added space above the first row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    icon: Icons.dashboard,
-                    text: 'Dashboard',
-                    count: '1',
-                    color: Colors.purple[100]!,
-                  ),
-                ),
-                SizedBox(width: 10), // Added space between the cards
-                Expanded(
-                  child: CardWidget(
-                    icon: Icons.people,
-                    text: 'Employees',
-                    count: '100',
-                    color: Colors.purple[100]!,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10), // Added space between the rows
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: CardWidget(
-                    icon: Icons.shopping_basket,
-                    text: 'Products',
-                    count: '50',
-                    color: Colors.purple[100]!,
-                  ),
-                ),
-                SizedBox(width: 10), // Added space between the cards
-                Expanded(
-                  child: CardWidget(
-                    icon: Icons.attach_money,
-                    text: 'Total Revenue',
-                    count: '\$10000',
-                    color: Colors.purple[100]!,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CardWidget extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final String count;
-  final Color color;
-
-  CardWidget({
-    required this.icon,
-    required this.text,
-    required this.count,
-    required this.color,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 40.0,
-              color: Colors.black,
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              text,
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 2.0),
-            Text(
-              count,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.black54,
-              ),
-            ),
-          ],
         ),
       ),
     );
